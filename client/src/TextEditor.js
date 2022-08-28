18c2b8ee944cfd8e80db087c97092573dfaf0078
mohmousad <moh.moussad@gmail.com>
2022-08-29 01:47:39 +0200

    edit

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:3001");
+    const s = io("https://google-docs-clone-server-101.herokuapp.com/");
     setSocket(s);
 
     return () => {
