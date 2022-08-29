5bd889f628e1455115eb05a0607fa29460f7f5e8
mohmousad <moh.moussad@gmail.com>
2022-08-29 22:07:40 +0200

    remove vercel links

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("https://google-docs-clone-server-101.herokuapp.com/");
+    const s = io("http://localhost:3001");
     setSocket(s);
 
     return () => {
